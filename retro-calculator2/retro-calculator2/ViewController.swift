2f9f3b514df0bdd742420048c5fecde1d9977df3
Kevin Amiranoff <kamiranoff@gmail.com>
2016-07-05 01:53:44 +0200

    adds wolverine-calculator

---
+++ retro-calculator2/retro-calculator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  retro-calculator2
+//
+//  Created by Kevin Amiranoff on 01/07/2016.
+//  Copyright © 2016 Kevin Amiranoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
